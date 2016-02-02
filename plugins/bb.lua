702033f71cd69b7eb70933c7fe6828dd2d2cfe99
mrbangi <xboxlivemnasr@gmail.com>
2016-02-02 19:41:34 +0330

    Create bb.lua

---
+++ plugins/bb.lua
@@ -0,0 +1,10 @@
+do
+ local function run(picture)
+ if picture.format == "jpg" then
+   --chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+   return "no pictures here"
+   patterns{
+       "^[Jj]pg
+       "^[!/]jp
+      end 
+        end
