bdad799abe5b1182c765febd03ff103708415533
mrbangi <xboxlivemnasr@gmail.com>
2016-02-04 09:41:53 +0330

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -1116,8 +1116,8 @@ local function run(msg, matches)
       if not is_momod(msg) then
         return "For moderators only!"
       end
-      if tonumber(matches[2]) < 5 or tonumber(matches[2]) > 20 then
-        return "Wrong number,range is [5-20]"
+      if tonumber(matches[2]) < 1 or tonumber(matches[2]) > 20 then
+        return "Wrong number,range is [1-20]"
       end
       local flood_max = matches[2]
       data[tostring(msg.to.id)]['settings']['flood_msg_max'] = flood_max
