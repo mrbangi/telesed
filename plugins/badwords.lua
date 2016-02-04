1f188d9c4979e1dd2d050198651ba2f680dfb4e4
mrbangi <xboxlivemnasr@gmail.com>
2016-02-04 08:58:51 +0330

    Delete badwords.lua

--- plugins/badwords.lua
+++
@@ -1,8 +0,0 @@
-do 
- function run(msg, matches)
- if msg.text == "kos" then
- return " dont send bad words again"
- if matches[1] == 'kos' then 
- chat_del_user(user#id, chat_del_usr, ok_cb, false)
- end
-  end
